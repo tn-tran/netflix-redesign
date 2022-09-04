59c344af37f93d8240f9b45c9253f646fc7fc0ea
Tien <tientran.developer@gmail.com>
2022-09-04 14:06:03 -0700

    Add API key

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Header */}
       <main>
-        {/* banner */}
+        <Banner />
         <section>
           {/* rows */}
         </section>
