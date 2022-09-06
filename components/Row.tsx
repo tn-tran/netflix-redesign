951d41292f5c5960b135f1e97245d4f4fd22aff8
Tien <tientran.developer@gmail.com>
2022-09-05 17:19:04 -0700

    Fix section row padding

--- components/Row.tsx
+++ components/Row.tsx
@@ -27,6 +27,8 @@ function Row({ title, movies }: Props) {
 		}
 	}
 
+	console.log(rowRef.current?.scrollLeft, rowRef.current?.clientWidth)
+
 	return (
 		<div className="h-40 space-y-0.5 md:space-y-2">
 			<h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
@@ -39,7 +41,6 @@ function Row({ title, movies }: Props) {
 					}`}
 					onClick={() => handleClick('left')}
 				/>
-
 				<div
 					className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
 					ref={rowRef}
@@ -49,7 +50,7 @@ function Row({ title, movies }: Props) {
 					))}
 				</div>
 				<ChevronRightIcon
-					className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+					className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
 					onClick={() => handleClick('right')}
 				/>
 			</div>
